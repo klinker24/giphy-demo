4268dd2706886c68e62381cc3af527b5ab214b98
Luke Klinker <lklinker1@gmail.com>
2016-07-21 11:00:14 -0500

    removed code to implement for part 4

--- app/src/main/java/xyz/lklinker/giphy_demo/task/GiphyTask.java
+++ app/src/main/java/xyz/lklinker/giphy_demo/task/GiphyTask.java
@@ -24,13 +24,17 @@ public class GiphyTask extends AsyncTask<Void, Void, List<Gif>> {
         this.api = new GiphyApi();
     }
 
-    @Override
-    protected List<Gif> doInBackground(Void... arg0) {
-        return api.queryApi(queryUri);
-    }
+    // android will crash if you try to perform network calls on the UI thread. Since these calls are
+    // blocking, they would force the app to be suspended, which isn't something the system allows
 
-    @Override
-    protected void onPostExecute(List<Gif> result) {
-        callback.onResponse(result);
-    }
+    // we want to add a doInBackground method and an onPostExecute method
+    // these two methods are used in an async task to perform callbacks to the UI.
+    // after your background method finishes, it will return whatever arguments it has created
+    // to the onPostExecute method.
+
+    // the return for the doInBackground method is defined by the last argument of the templated AsyncTask implementation
+    // The arguments for the onPostExecute method correspond to what is returned by the background method.
+
+
+    // use the api and the callback objects to implement these two methods.
 }
