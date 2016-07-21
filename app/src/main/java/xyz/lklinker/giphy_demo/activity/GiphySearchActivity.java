d0c65edc9a2b49e50092b097b044414b6f54bf6e
Luke Klinker <lklinker1@gmail.com>
2016-07-21 10:47:35 -0500

    remove what should be implemented for part 6

--- app/src/main/java/xyz/lklinker/giphy_demo/activity/GiphySearchActivity.java
+++ app/src/main/java/xyz/lklinker/giphy_demo/activity/GiphySearchActivity.java
@@ -10,30 +10,29 @@ import xyz.lklinker.giphy_demo.activity.base.GiphyActivity;
 
 public class GiphySearchActivity extends GiphyActivity {
 
-    private static final String ARG_SEARCH_QUERY = "arg_search_query";
-
     /**
      * Static factory method to create a search intent.
      */
     public static Intent createIntent(Context context, String searchQuery) {
-        Intent intent = new Intent(context, GiphySearchActivity.class);
-        intent.putExtra(ARG_SEARCH_QUERY, searchQuery);
-
-        return intent;
+        // create a new intent for this activity here.
+        // you can add an extra to an intent to pass parameters between two activities.
+        // use that to send the searchQuery
     }
+
+    private String searchQuery;
 
     @Override
     public void onPostCreate(Bundle savedInstanceState) {
         super.onPostCreate(savedInstanceState);
-        getToolbar().setTitle(getIntent().getStringExtra(ARG_SEARCH_QUERY));
+
+        // first fill the searchQuery field
+
+
+        // then, lets set the toolbar's title to the query we just loaded
     }
 
     @Override
     protected String getGiphyQueryUrl() {
-        GiphyQueryBuilder builder = new GiphyQueryBuilder(GiphyQueryBuilder.EndPoint.SEARCH)
-                .setQuery(getIntent().getStringExtra(ARG_SEARCH_QUERY))
-                .setLimit(100);
-
-        return builder.build();
+        // use the GiphyQueryBuilder to return a query for the search endpoint with a limit of 100
     }
 }
